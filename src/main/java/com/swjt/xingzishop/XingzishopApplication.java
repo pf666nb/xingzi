3862269e4c6154b78b69a9a7b918862185728208
wpf <1340701723@qq.com>
2021-04-12 22:17:56 +0800

    杏子商场Initial commit

---
+++ src/main/java/com/swjt/xingzishop/XingzishopApplication.java
@@ -0,0 +1,13 @@
+package com.swjt.xingzishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XingzishopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XingzishopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swjt/xingzishop/XingzishopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swjt.xingzishop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XingzishopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
